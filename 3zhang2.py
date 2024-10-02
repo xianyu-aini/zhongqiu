24c212f6c6ab19961a6f166d4033ddd6fca91db8
xianyu-aini <2791675149@qq.com>
2024-10-02 16:34:22 +0800

    Add files via upload

---
+++ 3zhang2.py
@@ -0,0 +1,9 @@
+nianling=int(input("请输入年龄："))
+if nianling<3:
+    print("婴儿")
+elif nianling<13:
+    print("儿童")
+elif nianling<18:
+    print("青少年")
+else:
+    print("成年人14")
---
+++ juxing.py
@@ -0,0 +1,4 @@
+kuan=int(input("请输入矩形的宽："))
+chang=int(input("请输入矩形的长："))
+mianji=kuan*chang
+print("矩形的面积：",mianji)
